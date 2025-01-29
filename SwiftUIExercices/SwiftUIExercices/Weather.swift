da887a46bc36faf4dacf7f6ee1e70a66f3fe566d
RushiTori <luciefontaine0507@gmail.com>
2025-01-29 10:55:09 +0100

    completed the FruitList exercice

--- SwiftUIExercices/SwiftUIExercices/Weather.swift
+++ SwiftUIExercices/SwiftUIExercices/Weather.swift
@@ -14,9 +14,11 @@ struct Weather: Identifiable {
     var color: Color
 }
 
-let weathers: [Weather] = [Weather(name: "Sunny", symbol: "sun.max", color: .yellow),
-                           Weather(name: "Clouds", symbol: "cloud.fill", color: .blue),
-                           Weather(name: "Storm", symbol: "cloud.bolt.fill", color: .gray),
-                           Weather(name: "Moon", symbol: "moon.fill", color: .purple),
-                           Weather(name: "Snow", symbol: "snowflake", color: .cyan),
-                           Weather(name: "Tornado", symbol: "tornado", color: .red)]
+let weathers: [Weather] = [
+    Weather(name: "Sunny", symbol: "sun.max", color: .yellow),
+    Weather(name: "Clouds", symbol: "cloud.fill", color: .blue),
+    Weather(name: "Storm", symbol: "cloud.bolt.fill", color: .gray),
+    Weather(name: "Moon", symbol: "moon.fill", color: .purple),
+    Weather(name: "Snow", symbol: "snowflake", color: .cyan),
+    Weather(name: "Tornado", symbol: "tornado", color: .red)
+]
