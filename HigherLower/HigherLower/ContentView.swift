b55b898713f61b1c0fcb7ef09b4ea096ec68ce29
RushiTori <luciefontaine0507@gmail.com>
2025-01-29 09:04:04 +0100

    trying to fix the gitignore

--- HigherLower/HigherLower/ContentView.swift
+++ HigherLower/HigherLower/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    var minRand: UInt64 = 0
+    var maxRand: UInt64 = 0
+    var goal: UInt64 = 0
+    var guess: UInt64 = 0
+    
+    @State var minRandText: String = "0"
+    @State var maxRandText: String = "10000"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Define boundaries (no negative numbers)")
+                .font(.largeTitle)
+                .foregroundStyle(.pink)
+            Spacer()
+            HStack {
+                Spacer()
+                TextField(text: $minRandText) {
+                    // WIP
+                }
+                .frame(width: 128)
+                .padding(.horizontal)
+                TextField(text: $maxRandText) {
+                    // WIP
+                }
+                .frame(width: 128)
+                .padding(.horizontal)
+                Spacer()
+            }
+            Spacer()
         }
-        .padding()
     }
 }
 
