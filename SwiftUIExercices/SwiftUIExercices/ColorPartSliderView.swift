4752bf9173a18e6c62f83f83ea5898bad5a14c24
RushiTori <luciefontaine0507@gmail.com>
2025-01-29 09:16:01 +0100

    added a preview for the struct ColorPartSliderView

--- SwiftUIExercices/SwiftUIExercices/ColorPartSliderView.swift
+++ SwiftUIExercices/SwiftUIExercices/ColorPartSliderView.swift
@@ -23,3 +23,14 @@ struct ColorPartSliderView: View {
         .font(.title3)
     }
 }
+
+#Preview {
+    @Previewable @State var backGray: Double = 50
+    ZStack {
+        Color(red: backGray / 255, green: backGray / 255, blue: backGray / 255, opacity: 1)
+            .ignoresSafeArea()
+        
+        ColorPartSliderView(colorPart: $backGray, partName: "Gray Scale")
+            .padding(.horizontal, 32)
+    }
+}
