01ed5ee4236e3f6a5b8abce162fff1d5da170fb9
RushiTori <luciefontaine0507@gmail.com>
2025-01-29 09:02:59 +0100

    updated gitignore

---
+++ HigherLower/HigherLower/HigherLowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HigherLowerApp.swift
+//  HigherLower
+//
+//  Created by apprenant97 on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct HigherLowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
